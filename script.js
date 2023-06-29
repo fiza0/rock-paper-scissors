34a74e7af0f26ac786047988083f5c79b87d59f4
fiza0 <issafuad05@gmail.com>
2023-06-29 11:01:53 +0300

    Make function to play one round
    player and computer choices are stored in their respective variables
    and comparisons are made to determine the result and log a result
    message

--- script.js
+++ script.js
@@ -10,8 +10,45 @@ function getComputerChoice() {
     }
     return computerSelection
 }
-function getUserChoice() {
+function getPlayerChoice() {
     let playerSelection
     playerSelection=prompt('choose your hand!').toLowerCase()
     return playerSelection
+}
+function playRound() {
+    let computerSelection=getComputerChoice()
+    let playerSelection=getPlayerChoice()
+    let resultMessage
+    let result
+    if(computerSelection===playerSelection){
+        resultMessage='It\'s a tie!'
+        result='tie'
+    } else if (playerSelection==='rock'&&computerSelection==='scissors') {
+        resultMessage='You win! rock beats scissors'
+        result='win'
+    } else if (playerSelection==='scissors'&&computerSelection==='rock') {
+        resultMessage='You lose! rock beats scissors'
+        result='loss'
+    } else if (playerSelection==='paper'&&computerSelection=='rock') {
+        resultMessage='You win! paper beats rock'
+        result='win'
+    } else if (playerSelection==='rock'&&computerSelection==='paper') {
+        resultMessage='You lose! paper beats rock'
+        result='loss'
+    } else if(playerSelection==='scissors'&&computerSelection==='paper') {
+        resultMessage='You win! scissors beats rock'
+        result='win'
+    } else if(playerSelection==='paper'&&computerSelection==='scissors'){
+        resultMessage='You lose! rock beats scissors'
+        result='loss'
+    } else{
+        resultMessage='not a real hand!'
+    }
+console.log(resultMessage)
+return result
+}
+function game(){
+for (i=0;i<=5;i++){
+    
+}
 }
