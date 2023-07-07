32585433a5024d6b3fe898e0a710874b1786f315
fiza0 <issafuad05@gmail.com>
2023-07-07 21:05:58 +0300

    Make function that assigns button a hand based on ID

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 //The zero index is the hand, the first index is what it beats
+
 let rock=['rock','scissors']
 let paper=['paper','rock']
 let scissors=['scissors','paper']
 //generate either 0,1 or 2 randomly
 //assign a hand based on r.n.g
+
 function getComputerSelection(){
     let computerSelection=Math.floor(Math.random()*3)
     if(computerSelection===0)computerSelection=rock
     if(computerSelection===1)computerSelection=paper
     if(computerSelection===2)computerSelection=scissors
-    console.log(computerSelection)
+    return computerSelection
+}
+//for each button, assign a player selection based on the id
+
+function getPlayerSelection(){
+    let buttons=document.querySelectorAll('.btn')
+    buttons.forEach(button=>{
+        let playerSelection
+        let id=button.id
+        if(id==='rock')playerSelection=rock
+        if(id==='paper')playerSelection=paper
+        if(id==='scissors')playerSelection=scissors
+    })
+    return playerSelection
 }
